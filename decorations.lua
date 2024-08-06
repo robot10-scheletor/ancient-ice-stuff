51a9c340f081fd44b0a44ecb730c6e59a495e50a
robot10 <--->
2024-08-06 18:17:09 +0200

    (checkpoint) the whole mod files (still not working)

---
+++ decorations.lua
@@ -0,0 +1,12 @@
+minetest.register_decoration({
+    deco_type = "schematic",
+    place_on = {"dirt_with_grass"},
+    sidelen = 9,
+    fill_ratio = 0.00003,
+    biomes = {"grassland"},
+    y_max = 200,
+    y_min = 1,
+    schematic = minetest.get_modpath("stalhrim") .. "/schematics/tomb_ruin.mts",
+    flags = "place_center_x, place_center_z",
+    rotation = "random",
+})
