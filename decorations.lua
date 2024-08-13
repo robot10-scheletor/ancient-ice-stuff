03975a9fbd65722d5480967e1cdeaf1abcbf1148
robot10 <--->
2024-08-13 15:29:48 +0200

    Added a new type of tomb

--- decorations.lua
+++ decorations.lua
@@ -10,3 +10,16 @@ minetest.register_decoration({
     flags = "place_center_x, place_center_z",
     rotation = "random",
 })
+
+minetest.register_decoration({
+    deco_type = "schematic",
+    place_on = {"dirt_with_grass"},
+    sidelen = 9,
+    fill_ratio = 0.00001,
+    biomes = {"grassland"},
+    y_max = 200,
+    y_min = 1,
+    schematic = minetest.get_modpath("ancient_ice_stuff") .. "/schematics/ancient_tomb.mts",
+    flags = "place_center_x, place_center_z",
+    rotation = "random",
+})
